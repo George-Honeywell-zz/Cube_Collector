e0e4f99ae0c9abb2c46030b5f45283732191f95c
George Honeywell <up843814@myport.ac.uk>
2020-08-31 16:47:41 +0100

    Fixed a bug that was causing the program to hang on start-up

--- Assets/Scripts/Spawn_Manager.cs
+++ Assets/Scripts/Spawn_Manager.cs
@@ -9,16 +9,17 @@ public class Spawn_Manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Green Enemy Instansiated");
-        Instantiate(Green_Enemy_Prefab, new Vector3(Random.Range(-4.5f, 4.5f), 0, 0), Quaternion.identity);
+        //Debug.Log("Green Enemy Instansiated");
+        //Instantiate(Green_Enemy_Prefab, new Vector3(Random.Range(-4.5f, 4.5f), 0, 0), Quaternion.identity);
 
-        //int spawned = 0;
+        int spawned = 0;
 
-        //while(spawned < 2)
-        //{
-        //    spawn_position = new Vector3(Random.Range(-4.5f, 4.5f), 6.0f, 0);
-        //    Instantiate(Green_Enemy_Prefab, spawn_position, Quaternion.identity);
-        //}
+        while(spawned < 2)
+        {
+            spawn_position = new Vector3(Random.Range(-4.5f, 4.5f), 6.0f, 0);
+            Instantiate(Green_Enemy_Prefab, spawn_position, Quaternion.identity);
+            spawned++;
+        }
 
     }
 
