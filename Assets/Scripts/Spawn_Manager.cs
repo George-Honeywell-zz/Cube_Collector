ae652cedcb2aa602b1e988671f35bfdfeb609c9f
George Honeywell <up843814@myport.ac.uk>
2020-08-31 16:16:21 +0100

    Basic mechanics added
    Created a green enemy to use as a base for mechanics.

---
+++ Assets/Scripts/Spawn_Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Manager : MonoBehaviour
+{
+    public GameObject Green_Enemy_Prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Green Enemy Instansiated");
+        Instantiate(Green_Enemy_Prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
