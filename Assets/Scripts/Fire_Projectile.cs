a73e924484901d98a5140957380ef578b11c073d
George Honeywell <up843814@myport.ac.uk>
2020-08-31 16:44:29 +0100

    Cube Spawning functionality implemented

---
+++ Assets/Scripts/Fire_Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire_Projectile : MonoBehaviour
+{
+    public Rigidbody projectile;
+    public float fire_speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody p = Instantiate(projectile, transform.forward, transform.rotation);
+            p.velocity = transform.up * fire_speed;
+        }
+    }
+}
--- Assets/Scripts/Spawn_Manager.cs
+++ Assets/Scripts/Spawn_Manager.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class Spawn_Manager : MonoBehaviour
 {
     public GameObject Green_Enemy_Prefab;
+    public Vector3 spawn_position;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Green Enemy Instansiated");
-        Instantiate(Green_Enemy_Prefab, new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(Green_Enemy_Prefab, new Vector3(Random.Range(-4.5f, 4.5f), 0, 0), Quaternion.identity);
+
+        //int spawned = 0;
+
+        //while(spawned < 2)
+        //{
+        //    spawn_position = new Vector3(Random.Range(-4.5f, 4.5f), 6.0f, 0);
+        //    Instantiate(Green_Enemy_Prefab, spawn_position, Quaternion.identity);
+        //}
+
     }
 
     // Update is called once per frame
