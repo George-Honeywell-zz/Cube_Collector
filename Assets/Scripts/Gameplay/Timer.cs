7d1be1732b0d0ceef072904cad2c625c86a315e2
George Honeywell <up843814@myport.ac.uk>
2020-09-04 22:34:12 +0100

    Implemented a timer

---
+++ Assets/Scripts/Gameplay/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float time_left = 30.0f;
+    public Text time_text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time_left > 0)
+        {
+            time_left -= Time.deltaTime;
+            Debug.Log(time_left);
+            time_text.text = time_left.ToString("00");
+        }
+    }
+}
