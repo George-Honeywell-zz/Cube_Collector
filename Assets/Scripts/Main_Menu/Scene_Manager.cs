45fcfad5447d96a6564dac48c1721b2ec919aae4
George Honeywell <up843814@myport.ac.uk>
2020-09-04 22:17:25 +0100

    Implemented a Main Menu & General project clean up

---
+++ Assets/Scripts/Main_Menu/Scene_Manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public GameObject Main_Menu;
+    public GameObject About_Button;
+    public GameObject Back_Button;
+
+
+    public void Change_Scene(int scene_id)
+    {
+        SceneManager.LoadScene(scene_id);
+    }
+
+    public void About()
+    {
+        About_Button.SetActive(true);
+        Main_Menu.SetActive(false); 
+    }
+
+    public void Back()
+    {
+        Main_Menu.SetActive(true);
+        About_Button.SetActive(false);
+    }
+
+    public void GitHub_Link()
+    {
+        Application.OpenURL("https://github.com/George-Honeywell/DirectX_Project");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
